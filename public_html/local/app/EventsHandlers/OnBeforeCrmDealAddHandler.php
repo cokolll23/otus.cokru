e3fed6c2dba0ff9fb90ac45a089dd1883b7077cd
ALEX <cavjob@ya.ru>
2025-08-19 22:12:46 +0300

    Расширение js обработчики

--- public_html/local/app/EventsHandlers/OnBeforeCrmDealAddHandler.php
+++ public_html/local/app/EventsHandlers/OnBeforeCrmDealAddHandler.php
@@ -7,6 +7,10 @@ use Bitrix\Iblock\IblockTable;
 use Bitrix\Main\Diag\Debug;
 use Bitrix\Iblock\ElementTable;
 use \Bitrix\Crm\Service\Container;
+use Bitrix\Crm\DealTable;
+use Bitrix\Main\Entity;
+
+Loader::includeModule('crm');
 
 
 Loader::includeModule('iblock');
@@ -17,6 +21,29 @@ class OnBeforeCrmDealAddHandler
 {
     public static function OnBeforeCrmDealAddHandler(&$arFields)
     {
+        // Здесь можно добавить условия, например:
+        $vin = $arFields["UF_CRM_DEAL_VIN"];
+        $arDeals = DealTable::getList([
+            'filter' => [ '=UF_CRM_DEAL_VIN' => $vin ,
+                '!=STAGE_ID' => 'WON'],
+            'select' => [
+                'ID',
+
+            ],
+            'order' => [],
+        ])->fetchAll();
+
+        Debug::dumpToFile($arDeals, '$arDeals ' . date('d-m-Y; H:i:s'));
+
+        if ($arDeals && count($arDeals)>0) {
+            $GLOBALS['APPLICATION']->ThrowException("Сделка не может быть создана! Проверьте заполнение полей");
+            return false;
+        }
+
+        // Запрещаем добавление сделки
+       /* ui-entity-section-control-error-block ui-entity-section-control-error-text
+        *  $GLOBALS['APPLICATION']->ThrowException("Сделка не может быть создана! Проверьте заполнение полей");
+        return false;*/
 
 
     }
--- public_html/local/php_interface/includes/getRandomNumbers.php
+++ public_html/local/php_interface/includes/getRandomNumbers.php
@@ -1,39 +1,38 @@
 <?php
+$_SERVER["DOCUMENT_ROOT"] = "/home/c/cok23/cokru24/public_html";
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+
+$params = [
+    'num' => 5,       // сколько чисел нам нужно
+    'min' => 0,          // минимальное значение (можно изменить)
+    'max' => 10,       // максимальное значение (можно изменить)
+    'col' => 1,          // количество колонок
+    'base' => 10,         // система счисления (10 - десятичная)
+    'format' => 'plain',    // формат ответа
+    'rnd' => 'new'       // гарантия случайности
+];
+
+$url = 'https://www.random.org/integers/?' . http_build_query($params);
+
+$response = file_get_contents($url);
+
+if ($response === false) {
+    // Обработка ошибки
+    die('Не удалось получить данные с random.org');
+}
+// Преобразуем ответ в массив чисел
+$randomNumbers = explode("\n", trim($response));
+Bitrix\Main\Diag\ Debug::dumpToFile($randomNumbers, '$randomNumbers ' . date('d-m-Y; H:i:s'));
 
 
-    $params = [
-        'num'    => 10,       // сколько чисел нам нужно
-        'min'    => 0,          // минимальное значение (можно изменить)
-        'max'    => 10,       // максимальное значение (можно изменить)
-        'col'    => 1,          // количество колонок
-        'base'   => 10,         // система счисления (10 - десятичная)
-        'format' => 'plain',    // формат ответа
-        'rnd'    => 'new'       // гарантия случайности
-    ];
 
-    $url = 'https://www.random.org/integers/?' . http_build_query($params);
-
-    $response = file_get_contents($url);
-
-    if ($response === false) {
-        // Обработка ошибки
-        die('Не удалось получить данные с random.org');
-    }
-    // Преобразуем ответ в массив чисел
-    $randomNumbers = explode("\n", trim($response));
-    // Bitrix\Main\Diag\ Debug::dumpToFile($randomNumbers, '$randomNumbers ' . date('d-m-Y; H:i:s'));
+$log = date('Y-m-d H:i:s') . ' ' . json_encode($randomNumbers, JSON_UNESCAPED_UNICODE);
+file_put_contents($_SERVER["DOCUMENT_ROOT"] . '/log.txt', $log . PHP_EOL, FILE_APPEND);
 
 
+$strGetFileNumbs = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/log.txt');
 
-    $log = date('Y-m-d H:i:s') . ' ' . print_r($randomNumbers, true);
-    file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/log.txt', $log . PHP_EOL, FILE_APPEND);
-
-    /*$log =  print_r(json_encode($randomNumbers), true);
-    file_put_contents( $_SERVER['DOCUMENT_ROOT'].'/log.txt', $log );*/
-
-    $strGetFileNumbs = file_get_contents( $_SERVER['DOCUMENT_ROOT'].'/log.txt' );
-
-    echo json_encode($randomNumbers,JSON_UNESCAPED_UNICODE);
+echo json_encode($randomNumbers, JSON_UNESCAPED_UNICODE);
 
 
 
